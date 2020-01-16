b34f65aa8c2d53cb0f16166ae7ad0174977af169
Dan Filla <danfilla@gmail.com>
2020-01-15 19:29:20 -0600

    The game works fine now. all that is left to do is to make it into an .exe

--- webscraper.py
+++ webscraper.py
@@ -1,15 +1,8 @@
 from bs4 import BeautifulSoup
 import requests
 
-def get_next_paragraph(count):
-	#toclevel-1 tocsection-'count'
-	next_paragraph_title = toctitle.find('li', class_='toclevel-1 tocsection-' + str(count)).text[2:]
-
-
-
-
-points = 1
-while 1:
+points = 0
+while exit:
 	# Filter through wiki pages until we find an artical with a contents page.
 	try:
 		req = requests.get("https://en.wikipedia.org/wiki/Special:Random").text
@@ -22,7 +15,6 @@ while 1:
 	# replaced by '-'
 	answer = soup.find('h1').text
 	answer_list = answer.lower().split()
-	print(answer)
 	first_paragraph = soup.find('div', id='mw-content-text').p.text
 
 	# Make sure that the first_paragraph is not empty.
@@ -37,9 +29,20 @@ while 1:
 		while 1:
 			user_input = input('What is your guess?...').lower()
 			if user_input == 'next':
-				points += 1
+				points += 5
 				print('The correct answer is ' + answer)
 				print('You have ' + str(points) + ' points.')
+				break
+			elif user_input.isdigit():
+				points += int(user_input)
+				print(str(user_input) + ' have been added to your score.')
+				print('You now have a score of ' + str(points))
+			elif user_input == 'q':
+				exit = False
+				print('You have a score of ' + str(points))
+				break
+			elif len(user_input) <= 2:
+				print('you have to guess a longer word...')
 				continue
 			elif user_input == answer.lower():
 				points -= 10
@@ -47,11 +50,14 @@ while 1:
 				print('Your score is ' + str(points))
 				break
 			elif user_input in answer_list:
+				points += 1
 				print("Alright close enough, I'll give it to you...")
 				print('The actuall answer is ' + answer)
 				print('Your score is ' + str(points))
 				break
 			else:
+				points += 2
+				print('Wrong!!')
 				continue
 	else:
 		continue
