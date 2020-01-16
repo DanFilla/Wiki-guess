c5ceda0d5c57093c10425e2a5c2c278a961af52e
Dan Filla <danfilla@gmail.com>
2020-01-15 18:04:00 -0600

    Game is working. Need to put in the point system and hints.

--- webscraper.py
+++ webscraper.py
@@ -1,38 +1,78 @@
 from bs4 import BeautifulSoup
 import requests
 
+def get_next_paragraph(count):
+	#toclevel-1 tocsection-'count'
+	next_paragraph_title = toctitle.find('li', class_='toclevel-1 tocsection-' + str(count)).text[2:]
 
 
 
 
-url = "https://en.wikipedia.org/wiki/Special:Random"
-url2 = "https://en.wikipedia.org/wiki/John_F._Cassidy"
-url3 = "https://en.wikipedia.org/wiki/George_Hotz"
-
-# print(soup.prettify())
-
-
+para = 1
 while 1:
+	# Filter through wiki pages until we find an artical with a contents page.
 	try:
 		req = requests.get("https://en.wikipedia.org/wiki/Special:Random").text
 		soup = BeautifulSoup(req, 'lxml')
 		toctitle = soup.find('div', class_='toctitle')
-
 	except AttributeError:
 		continue
 
-	title = soup.find('h1').text
+	# Display the first paragraph to the user with all of the answers 
+	# replaced by '-'
+	answer = soup.find('h1').text
+	answer_list = answer.lower().split()
+	print(answer)
 	first_paragraph = soup.find('div', id='mw-content-text').p.text
 
+	# Make sure that the first_paragraph is not empty.
+	if first_paragraph.strip() and len(answer_list) <= 3:
 
-	print(title)
+		for word in answer_list:
+			first_paragraph = first_paragraph.lower().replace(word, '-'.center(len(word), '-'))
 
-	for word in title.lower().split():
-		first_paragraph = first_paragraph.lower().replace(word, '-'.center(len(word), '-'))
+		print(first_paragraph)
+
+		# Loop for each wiki page the user can guess on.
+		while 1:
+			user_input = input('What is your guess?...').lower()
+			if user_input == 'next':
+				print(get_next_paragraph(para))
+				continue
+			elif user_input == answer.lower():
+				print('You got it correct!')
+				break
+			elif user_input in answer_list:
+				print("Alright close enough, I'll give it to you...")
+				print('The actuall answer is ' + answer)
+				break
+			else:
+				continue
+	else:
+		continue
 
 
 
-	print(first_paragraph)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
